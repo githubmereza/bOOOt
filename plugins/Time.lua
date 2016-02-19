e800435068287bbb233d2f9ecec4127235ee6c4d
githubmereza <github09355332076@gmail.com>
2016-02-19 19:54:37 +0330

    Update Time.lua

--- plugins/Time.lua
+++ plugins/Time.lua
@@ -95,6 +95,6 @@ end
 return {
   description = "Get Time Give by Local Name", 
   usage = "/time (local) : view local time",
-  patterns = {"^[!/]time (.*)$"}, 
+  patterns = {"^وقت (.*)$"}, 
   run = run
 }
